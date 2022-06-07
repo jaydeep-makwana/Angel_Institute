971cc2e4500373b8386de061b12f6c62f6966372
Priya8632 <pthummar775@gmail.com>
2022-06-07 12:33:22 +0530

    navbar

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>navbar</title>
+    <style>
+        .img{
+            display: flex;
+            flex-direction: row-reverse;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar">
+        <img src="iamges/angel.png" alt="" height="60px" width="200px">
+        <ul class="nav jastify-content-end">
+            <li class="nav-item"><a href="" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="" class="nav-link">About</a></li>
+            <li class="nav-item"><a href="" class="nav-link">Ragister</a></li>
+            <li class="nav-item"><a href="" class="nav-link">Login</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
