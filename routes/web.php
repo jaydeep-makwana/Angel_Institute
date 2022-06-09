d388d7efe915d8fb8105fba40a082cd2bdccc599
Priya8632 <pthummar775@gmail.com>
2022-06-09 21:24:23 +0530

    login design and ragister form

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('demo');
 });
-<<<<<<< HEAD
 Route::view('/', 'home');
 Route::view('admission', 'Admission_Form');
 Route::view('navbar','navbar');
 Route::view('admin_dashboard','admin_dashboard');
 Route::view('login','login');
 Route::view('ragister','ragister');
-=======
+
 Route::view('welcome', 'welcome');
 Route::view('navbar','navbar');
 Route::view('admin_dashboard','admin_dashboard');
 Route::post('admission_form',[admisssion_form::class,'operation']);
 Route::view('admission_form', 'Admission_Form');
->>>>>>> bc9abec9af3a77460dadb402e9a10c46d3f1b6aa
+
