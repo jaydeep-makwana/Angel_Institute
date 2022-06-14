ca78fec8eed626967e7197d2ad638b4e06b4fdf2
jaydeep-makwana <jobandstudypoint@gmail.com>
2022-06-13 20:48:11 -0700

    bday

--- app/Http/Controllers/operationController.php
+++ app/Http/Controllers/operationController.php
@@ -89,13 +89,7 @@ class operationController extends Controller
     {
         $birthday = DB::table('students')->where('BOD',date('Y-m-d'))->get();
   
-        if (count($birthday) != 0) {
-           return $birthday;
-
-        } else{
-            
-            return 'today is not have birth day of any student';
-        }
+      return view('Dashboard.birthday',['birthdays'=>$birthday]);
         
     }
     
