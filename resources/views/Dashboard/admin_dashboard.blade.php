7ac41e0768ae9f9c4cbdbcc1362a9330b912a92e
erroreyes <krisharadadiya20003@gmail.com>
2022-06-23 08:40:51 +0100

    fees payment

--- resources/views/Dashboard/admin_dashboard.blade.php
+++ resources/views/Dashboard/admin_dashboard.blade.php
@@ -20,7 +20,6 @@
         <a type="button" data-toggle="modal" data-target="#exampleModal" data-bs-whatever="@getbootstrap">Add course</a>
         <a href="#" id="batch">Batch</a>
         <a href="{{url('bDay')}}">Birthday</a>
-        <a type="button" data-toggle="modal" data-target="#staticBackdrop" data-bs-whatever="@getbootstrap">Payment</a>
         <a class="nav-link nav" href="{{ url('/') }}">Home </a>
         <a class="nav-link nav" href="{{ url('/admin_dashboard') }}">Dashboard </a>
         <a class="nav-link nav" href="{{ url('admin_logout') }}">Logout</a>
