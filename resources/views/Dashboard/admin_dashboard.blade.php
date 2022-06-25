230cfb83e8cace7b2933cbd1351561f7934d3ed3
erroreyes <krisharadadiya20003@gmail.com>
2022-06-25 06:41:59 +0100

    update

--- resources/views/Dashboard/admin_dashboard.blade.php
+++ resources/views/Dashboard/admin_dashboard.blade.php
@@ -168,24 +168,27 @@
                                         </button>
                                     </div>
                                     <div class="modal-body">
-                                        <form action="add_payment" method="POST">
-                                            @csrf
-                                            <label>Student Id :</label>
-                                            <input name="id" placeholder="id" class="form-control" value="{{$info->id}}">
-                                            <label for="">Name :</label>
-                                            <input name="Full_Name" placeholder="Full Name" class="form-control" value="{{$info->Full_Name}}">
-                                            <label for="">Course :</label>
-                                            <input type="text" name="Course" placeholder="XYZ" class="form-control" value="{{$info->Course}}">
-                                            <label for="">Date :</label>
-                                            <input type="date" name="date_of_payment" placeholder="Date" class="form-control">
-                                            <label for="">Payment :</label>
-                                            <input type="text" name="fees" placeholder="Fess" class="form-control">
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                <button type="submit" class="btn btn-primary">Submit</button>
-                                            </div>
-                                        </form>
-
+                                        <div class="container text-left">
+                                            <h4>
+                                                <p> Student Id : {{$info->id}}
+                                                </p>
+                                                <p>Name : {{$info->Full_Name}}
+                                                </p>
+                                                <p class="mb-4">Course : {{$info->Course}}
+                                                </p>
+                                            </h4>
+                                            <form action="add_payment" method="POST" >
+                                                @csrf
+                                                <label for="" style="font-size:large;">Date :</label>
+                                                <input type="date" name="date_of_payment" placeholder="Date" class="form-control mb-4">
+                                                <label for="" style="font-size:large ;">Payment :</label>
+                                                <input type="text" name="fees" placeholder="Fess" class="form-control">
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                                </div>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -207,33 +210,33 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <!-- Ends Modal for student details -->
+        </div>
+    </div>
+    <!-- Ends Modal for student details -->
 
-                <!-- modal for fees payment -->
-                <div class="modal fade" id="fees-{{$info->id}}" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-l">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="staticBackdropLabel">Add Payment</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <form action="add_payment" method="POST">
-                                    @csrf
-                                    <label>Student Id :</label>
-                                    <input name="id" placeholder="id" class="form-control" value="{{$info->id}}">
-                                    <label for="">Name :</label>
-                                    <input name="Full_Name" placeholder="Full Name" class="form-control" value="{{$info->Full_Name}}">
-                                    <label for="">Course :</label>
-                                    <input type="text" name="Course" placeholder="XYZ" class="form-control" value="{{$info->Course}}">
-                                    <label for="">Date :</label>
-                                    <input type="date" name="date_of_payment" placeholder="Date" class="form-control">
-                                    <label for="">Payment :</label>
-                                    <input type="text" name="fees" placeholder="Fess" class="form-control">
+    <!-- modal for fees payment -->
+    <div class="modal fade" id="fees-{{$info->id}}" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-l">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Add Payment</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="add_payment" method="POST">
+                        @csrf
+                        <label>Student Id :</label>
+                        <input name="id" placeholder="id" class="form-control" value="{{$info->id}}">
+                        <label for="">Name :</label>
+                        <input name="Full_Name" placeholder="Full Name" class="form-control" value="{{$info->Full_Name}}">
+                        <label for="">Course :</label>
+                        <input type="text" name="Course" placeholder="XYZ" class="form-control" value="{{$info->Course}}">
+                        <label for="">Date :</label>
+                        <input type="date" name="date_of_payment" placeholder="Date" class="form-control">
+                        <label for="">Payment :</label>
+                        <input type="text" name="fees" placeholder="Fess" class="form-control">
 
 
 
@@ -261,27 +264,27 @@
                                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                                         <button type="submit" class="btn btn-primary">Submit</button>
                                     </div>
-                                </form>
-                                    </form>
+                    </form>
+                    </form>
 
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Ends Modal for student details -->
+                </div>
+            </div>
+        </div>
+        <!-- Ends Modal for student details -->
 
-                <!--  Finish modal for fees payment -->
+        <!--  Finish modal for fees payment -->
 
-                <!-- modal for batch time -->
-                <div class="modal fade" id="batch-{{$info->id}}" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-l">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="staticBackdropLabel">{{$info->id}}</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
+        <!-- modal for batch time -->
+        <div class="modal fade" id="batch-{{$info->id}}" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-l">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="staticBackdropLabel">{{$info->id}}</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
 
 
                         <!-- modal for batch time -->
@@ -302,74 +305,74 @@
                             </div>
                         </div>
                         <!--  Finish Add Course Modal -->
-                    </tr>
-                </tbody>
-                @endforeach
-            </table>
-         
-        </div>
-    </div>
+                        </tr>
+                        </tbody>
+                        @endforeach
+                        </table>
 
-
-    <!-- Add Course Modal -->
-
-    <!-- Modal-->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Add Course</h5>
-                    <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+                    </div>
                 </div>
-                <div class="modal-body">
-                    <form action="add_course" method="POST">
-                        @csrf
-                        <div class="form-group">
-                            <label for="courseName" class="col-form-label">Course Name:</label>
-                            <input type="text" class="form-control" id="courseName" name="courseName">
+
+
+                <!-- Add Course Modal -->
+
+                <!-- Modal-->
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Add Course</h5>
+                                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <form action="add_course" method="POST">
+                                    @csrf
+                                    <div class="form-group">
+                                        <label for="courseName" class="col-form-label">Course Name:</label>
+                                        <input type="text" class="form-control" id="courseName" name="courseName">
+                                    </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                            </form>
+
                         </div>
+                    </div>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
-                </form>
+                <!--  Finish Add Course Modal -->
 
-            </div>
-        </div>
-    </div>
-    <!--  Finish Add Course Modal -->
+                <script>
+                    function openNav() {
+                        document.getElementById("mySidenav").style.width = "250px";
+                        document.getElementById("main").style.marginLeft = "250px";
+                    }
 
-    <script>
-        function openNav() {
-            document.getElementById("mySidenav").style.width = "250px";
-            document.getElementById("main").style.marginLeft = "250px";
-        }
+                    function closeNav() {
+                        document.getElementById("mySidenav").style.width = "0";
+                        document.getElementById("main").style.marginLeft = "0";
+                    }
+                </script>
 
-        function closeNav() {
-            document.getElementById("mySidenav").style.width = "0";
-            document.getElementById("main").style.marginLeft = "0";
-        }
-    </script>
+                <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+                <script>
+                    // $('#details').on("click", function() {
+                    //     $('#stu_details').modal('show');
+                    // });
+                    // $('#close-modal').on("click", function() {
+                    //     $('#stu_details').modal('hide');
+                    // });
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script>
-        // $('#details').on("click", function() {
-        //     $('#stu_details').modal('show');
-        // });
-        // $('#close-modal').on("click", function() {
-        //     $('#stu_details').modal('hide');
-        // });
-
-        function details(id) {
-            console.log(id);
-        }
-    </script>
+                    function details(id) {
+                        console.log(id);
+                    }
+                </script>
 </body>
 
 </html>
